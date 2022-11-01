f85d774028459eef606844c0e5655f8490368dd7
omar <omaralhoori@gmail.com>
2022-11-01 07:21:51 +0300

    confirmation statement

---
+++ erpnext/accounts/doctype/balance_confirmation_statement_print_format/balance_confirmation_statement_print_format.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Balance Confirmation Statement Print Format', {
+	// refresh: function(frm) {
+
+	// }
+});
