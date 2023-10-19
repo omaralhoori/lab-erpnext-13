017ade20d61f50f5ee7ed3e9b79548e149e3f3d9
Omar <omaralhoori@gmail.com>
2023-10-20 00:56:03 +0300

    api get with lab test

--- erpnext/api.py
+++ erpnext/api.py
@@ -2,8 +2,8 @@ import frappe
 from erpnext.healthcare.doctype.lab_test.lab_test_print import lab_test_result
 @frappe.whitelist(allow_guest=True)
 def patient_results(invoice, password):
-    patient = frappe.db.get_value("Sales Invoice", invoice, ['patient'])
+    patient = frappe.db.get_value("Lab Test", invoice, ['patient'])
     if not patient: return
     if frappe.db.get_value("Patient", patient, 'patient_password') != password: return
-    lab_test = frappe.db.get_value("Lab Test", {"sales_invoice": invoice}, "name")
-    return lab_test_result(lab_test, only_finilized=True, head=True)
+    #lab_test = frappe.db.get_value("Lab Test", {"sales_invoice": invoice}, "name")
+    return lab_test_result(invoice, only_finilized=True, head=True)
