303fde346aaed769cbea516bbd2678f9ccef5476
Omar <omaralhoori@gmail.com>
2024-01-23 22:34:28 +0300

    fix receivable_invoice report

--- erpnext/accounts/doctype/receivable_invoice/receivable_invoice.py
+++ erpnext/accounts/doctype/receivable_invoice/receivable_invoice.py
@@ -13,17 +13,30 @@ class ReceivableInvoice(Document):
 
 def get_conditions(invoice):
 	conditions = ""
-	conditions += f" AND company ='{invoice.company}'"
-	if invoice.from_date: conditions += f" AND posting_date >='{invoice.from_date}'"
-	if invoice.to_date: conditions += f" AND posting_date <='{invoice.to_date}'"
-	if invoice.insurance_party: conditions += f" AND (insurance_party='{invoice.insurance_party}' or insurance_party_child='{invoice.insurance_party}')"
+	conditions += f" AND inv.company ='{invoice.company}'"
+	if invoice.from_date: conditions += f" AND inv.posting_date >='{invoice.from_date}'"
+	if invoice.to_date: conditions += f" AND inv.posting_date <='{invoice.to_date}'"
+	if invoice.insurance_party: conditions += f" AND (inv.insurance_party='{invoice.insurance_party}' or inv.insurance_party_child='{invoice.insurance_party}')"
 	return conditions
 
-def get_invoices(conditions):
+def get_invoices_old(conditions):
 	invoices = frappe.db.sql(f"""
 		select name, posting_date, customer,
 		insurance_party,total_discount_provider -  discount_amount as company_net
 		from `tabSales Invoice`
 		where docstatus = 1 {conditions} order by posting_date , name """
+		 , as_dict=1)
+	return invoices
+def get_invoices(conditions):
+	invoices = frappe.db.sql(f"""
+		select inv.name, inv.posting_date, inv.customer,
+		inv.insurance_party,
+		SUM(invItem.discount_amount * invItem.qty) -  SUM(invItem.contract_discount) as company_net
+		from  `tabSales Invoice Item` as invItem
+		INNER JOIN `tabSales Invoice` as inv ON inv.name=invItem.parent
+		where inv.docstatus = 1 {conditions}
+		GROUP BY inv.name
+		order by inv.posting_date , inv.name 
+		"""
 		 , as_dict=1)
 	return invoices
